c9fd4ebb4b5f504a859c08c17056408885c1416b
immohammadnahali <mohammad.nahali@gmail.com>
2026-02-16 10:46:46 +0330

    add files
    اضافه کردن فایل های اولیه

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import requests
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+URL = f"https://api.telegram.org/bot{TOKEN}/"
+
+app = Flask(__name__)
+
+def send_message(chat_id, text):
+    requests.post(URL + "sendMessage", json={
+        "chat_id": chat_id,
+        "text": text
+    })
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot is running!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        if text == "سلام":
+            send_message(chat_id, "سلام 👋")
+
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
